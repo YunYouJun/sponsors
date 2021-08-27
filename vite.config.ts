030174aaf53c2113836d00145d638bb50fe2d058
YunYouJun <me@yunyoujun.cn>
2021-08-27 15:37:29 +0800

    🐛 can not build with vite-plugin-element-plus

--- vite.config.ts
+++ vite.config.ts
@@ -17,111 +17,114 @@ import VitePluginElementPlus from "vite-plugin-element-plus";
 const markdownWrapperClasses = "prose prose-sm m-auto text-left";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      "~/": `${path.resolve(__dirname, "src")}/`,
-    },
-  },
-
-  plugins: [
-    Vue({
-      include: [/\.vue$/, /\.md$/],
-    }),
-
-    VitePluginElementPlus({
-      // 如果你需要使用 [component name].scss 源文件，你需要把下面的注释取消掉。
-      // 对于所有的 API 你可以参考 https://github.com/element-plus/vite-plugin-element-plus
-      // 的文档注释
-      // useSource: true
-      format: "cjs",
-    }),
-
-    // https://github.com/hannoeru/vite-plugin-pages
-    Pages({
-      extensions: ["vue", "md"],
-    }),
-
-    // https://github.com/JohnCampionJr/vite-plugin-vue-layouts
-    Layouts(),
-
-    // https://github.com/antfu/vite-plugin-md
-    Markdown({
-      wrapperClasses: markdownWrapperClasses,
-      headEnabled: true,
-      markdownItSetup(md) {
-        // https://prismjs.com/
-        md.use(Prism);
+export default defineConfig(({ mode }) => {
+  return {
+    resolve: {
+      alias: {
+        "~/": `${path.resolve(__dirname, "src")}/`,
       },
-    }),
+    },
 
-    // https://github.com/antfu/vite-plugin-components
-    ViteComponents({
-      // allow auto load markdown components under `./src/components/`
-      extensions: ["vue", "md"],
+    plugins: [
+      Vue({
+        include: [/\.vue$/, /\.md$/],
+      }),
 
-      // allow auto import and register components used in markdown
-      customLoaderMatcher: (id) => id.endsWith(".md"),
+      VitePluginElementPlus({
+        // 如果你需要使用 [component name].scss 源文件，你需要把下面的注释取消掉。
+        // 对于所有的 API 你可以参考 https://github.com/element-plus/vite-plugin-element-plus
+        // 的文档注释
+        // useSource: true,
+        // 开发时使用 esm，打包时使用 cjs
+        format: mode === "development" ? "esm" : "cjs",
+      }),
 
-      // generate `components.d.ts` for ts support with Volar
-      globalComponentsDeclaration: true,
+      // https://github.com/hannoeru/vite-plugin-pages
+      Pages({
+        extensions: ["vue", "md"],
+      }),
 
-      // auto import icons
-      customComponentResolvers: [
-        // https://github.com/antfu/vite-plugin-icons
-        ViteIconsResolver(),
-        // disable for build, wait for https://github.com/antfu/vite-plugin-components/issues/58
-        // ElementPlusResolver(),
-      ],
-    }),
+      // https://github.com/JohnCampionJr/vite-plugin-vue-layouts
+      Layouts(),
 
-    // https://github.com/antfu/vite-plugin-icons
-    ViteIcons(),
+      // https://github.com/antfu/vite-plugin-md
+      Markdown({
+        wrapperClasses: markdownWrapperClasses,
+        headEnabled: true,
+        markdownItSetup(md) {
+          // https://prismjs.com/
+          md.use(Prism);
+        },
+      }),
 
-    // https://github.com/antfu/vite-plugin-windicss
-    WindiCSS({
-      safelist: markdownWrapperClasses,
-    }),
+      // https://github.com/antfu/vite-plugin-components
+      ViteComponents({
+        // allow auto load markdown components under `./src/components/`
+        extensions: ["vue", "md"],
 
-    // https://github.com/antfu/vite-plugin-pwa
-    VitePWA({
-      registerType: "autoUpdate",
-      manifest: {
-        name: "YunYouJun's Sponsors",
-        short_name: "Sponsors",
-        theme_color: "#ffffff",
-        icons: [
-          {
-            src: "/logo.png",
-            sizes: "240x240",
-            type: "image/png",
-          },
+        // allow auto import and register components used in markdown
+        customLoaderMatcher: (id) => id.endsWith(".md"),
+
+        // generate `components.d.ts` for ts support with Volar
+        globalComponentsDeclaration: true,
+
+        // auto import icons
+        customComponentResolvers: [
+          // https://github.com/antfu/vite-plugin-icons
+          ViteIconsResolver(),
+          // disable for build, wait for https://github.com/antfu/vite-plugin-components/issues/58
+          // ElementPlusResolver(),
         ],
+      }),
+
+      // https://github.com/antfu/vite-plugin-icons
+      ViteIcons(),
+
+      // https://github.com/antfu/vite-plugin-windicss
+      WindiCSS({
+        safelist: markdownWrapperClasses,
+      }),
+
+      // https://github.com/antfu/vite-plugin-pwa
+      VitePWA({
+        registerType: "autoUpdate",
+        manifest: {
+          name: "YunYouJun's Sponsors",
+          short_name: "Sponsors",
+          theme_color: "#ffffff",
+          icons: [
+            {
+              src: "/logo.png",
+              sizes: "240x240",
+              type: "image/png",
+            },
+          ],
+        },
+      }),
+
+      // https://github.com/intlify/vite-plugin-vue-i18n
+      VueI18n({
+        runtimeOnly: true,
+        compositionOnly: true,
+        include: [path.resolve(__dirname, "locales/**")],
+      }),
+    ],
+
+    server: {
+      fs: {
+        strict: true,
       },
-    }),
-
-    // https://github.com/intlify/vite-plugin-vue-i18n
-    VueI18n({
-      runtimeOnly: true,
-      compositionOnly: true,
-      include: [path.resolve(__dirname, "locales/**")],
-    }),
-  ],
-
-  server: {
-    fs: {
-      strict: true,
     },
-  },
 
-  // https://github.com/antfu/vite-ssg
-  ssgOptions: {
-    script: "async",
-    formatting: "minify",
-  },
+    // https://github.com/antfu/vite-ssg
+    ssgOptions: {
+      script: "async",
+      formatting: "minify",
+    },
 
-  optimizeDeps: {
-    include: ["vue", "vue-router", "@vueuse/core"],
-    exclude: ["vue-demi"],
-  },
+    optimizeDeps: {
+      include: ["vue", "vue-router", "@vueuse/core"],
+      exclude: ["vue-demi"],
+    },
+  };
 });
