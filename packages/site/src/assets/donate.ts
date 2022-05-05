a9d55409a29ae38b0629ba73d823a469e74c4aff
YunYouJun <me@yunyoujun.cn>
2022-05-06 01:52:34 +0800

    fix: update link to cdn.yunyoujun.cn to avoid wechat block

--- packages/site/src/assets/donate.ts
+++ packages/site/src/assets/donate.ts
@@ -8,18 +8,18 @@ export const donateMethod: {
 } = {
   ALI_PAY: {
     title: '支付宝',
-    url: 'https://cdn.jsdelivr.net/gh/YunYouJun/cdn/img/donate/alipay-qrcode.jpg',
+    url: 'https://cdn.yunyoujun.cn/img/donate/alipay-qrcode.jpg',
   },
   WECHAT_PAY: {
     title: '微信支付',
-    url: 'https://cdn.jsdelivr.net/gh/YunYouJun/cdn/img/donate/wechatpay-qrcode.jpg',
+    url: 'https://cdn.yunyoujun.cn/img/donate/wechatpay-qrcode.jpg',
   },
   WECHAT_REWARD: {
     title: '微信赞赏',
-    url: 'https://cdn.jsdelivr.net/gh/YunYouJun/cdn/img/donate/wechat-reward-code.jpg',
+    url: 'https://cdn.yunyoujun.cn/img/donate/wechat-reward-code.jpg',
   },
   QQ_PAY: {
     title: 'QQ 钱包',
-    url: 'https://cdn.jsdelivr.net/gh/YunYouJun/cdn/img/donate/qqpay-qrcode.png',
+    url: 'https://cdn.yunyoujun.cn/img/donate/qqpay-qrcode.png',
   },
 }
