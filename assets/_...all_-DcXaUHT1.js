1f1f7a4c555128ab48db9306359d7b89cff7448e
YunYouJun <YunYouJun@users.noreply.github.com>
2024-06-15 22:23:18 +0000

    deploy: 9e142203ec02751702e90cb51f2e830f6c8a6b66

---
+++ assets/_...all_-DcXaUHT1.js
@@ -0,0 +1,1 @@
+import{d as e,u as o,o as s,c as a,t as c,e as r}from"./app-CFx2obmD.js";const l=e({__name:"[...all]",setup(u){const{t}=o();return(f,p)=>(s(),a("div",null,c(r(t)("not-found")),1))}}),n={};typeof n=="function"&&n(l);export{l as default};
