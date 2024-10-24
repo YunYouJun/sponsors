5379b360c5a878b260da15c178302ff3b2f8dcbc
YunYouJun <me@yunyoujun.cn>
2024-10-24 14:48:03 +0800

    chore: add redirect link

---
+++ packages/site/src/pages/projects.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+onMounted(() => {
+  setTimeout(() => {
+    window.location.href = 'https://www.yunyoujun.cn/projects/'
+  }, 1000)
+})
+</script>
+
+<template>
+  <div class="my-40 items-center justify-center text-xl" flex="~ wrap" gap="4">
+    <span font="bold">
+      正在重定向至
+    </span>
+    <a class="text-blue-500 underline" href="https://www.yunyoujun.cn/projects/" target="_blank">
+      https://www.yunyoujun.cn/projects/
+    </a>
+  </div>
+</template>
